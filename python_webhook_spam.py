85778ec831473d9f791368c10f5be71a414b2daf
Botiefyed <109695906+Botiefyed@users.noreply.github.com>
2022-07-20 21:30:27 +0100

    Create python_webhook_spam.py

--- python_webhook_spam.py
+++ python_webhook_spam.py
@@ -1,21 +1,14 @@
-import time; import requests;
+import time; import requests; ##replace "your message here" with what you want to say, replace "your webhook link" with the discord webhook link.
 value = int(0)
 print("renember this is a webhook spammer not a dm spammer")
 exits = input("press anything to continue\ntype exit if you want to close this program\n")
 if exits : "exit" ; exit()
-print("\ntype what you want to spam")
-content_ = input() ##getting important info.
-print("\n paste webhook link here")
-webtoken = input()
 ##this is a webhook spammer, if u see this thank you for downloading this #gamer4life.
-def message(): ##this is message function.
-    value = value + int(1)
-    print("sent", value, "times")
-    payload = {
-        'content':content_ 
-    }
-    requests.post(webtoken, data=payload)
-    time.sleep(2) ##without this it would crash.
+
 for i in range(42069): ##how many messages to send :).
-    message ##using the function we created.
+  payload = {
+        'content':'_your message_'
+    }
+  requests.post('https://discord.com/api/webhooks/993104074155245568/rf6F0DBmqO8AXsR_xNOrrZ9nNzexYDha9lzTXYGDhJ2VAhruEDr7VBj2HttRGxEFP9cc', data=payload)
+  time.sleep(2) ##without this it would crash.
 ##thats the end. 
